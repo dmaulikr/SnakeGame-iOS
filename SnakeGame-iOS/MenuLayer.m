37a449bfde22464ac2fdaac706825f0ab0200e20
Dilip Muthukrishnan <dm_iyer@homtail.com>
2013-12-22 22:20:47 -0500

    Added more buttons to the menu, including a submenu of options to choose difficulty level

--- SnakeGame-iOS/MenuLayer.m
+++ SnakeGame-iOS/MenuLayer.m
@@ -29,8 +29,18 @@
         bg.position = ccp(160.0, 240.0);
         [self addChild:bg];
         playgame = [CCMenuItemImage itemFromNormalImage:@"playgame.png" selectedImage:@"playgame.png" target:self selector:@selector(playGameSelected:)];
-        playgame.position = ccp(160.0, 240.0);
-        CCMenu *menu = [CCMenu menuWithItems:playgame, nil];
+        playgame.position = ccp(160.0, 315.0);
+        highscores = [CCMenuItemImage itemFromNormalImage:@"highscores.png" selectedImage:@"highscores.png" target:self selector:@selector(highScoresSelected:)];
+        highscores.position = ccp(160.0, 240.0);
+        tutorial = [CCMenuItemImage itemFromNormalImage:@"tutorial.png" selectedImage:@"tutorial.png" target:self selector:@selector(tutorialSelected:)];
+        tutorial.position = ccp(160.0, 165.0);
+        easy = [CCMenuItemImage itemFromNormalImage:@"easy.png" selectedImage:@"easy.png" target:self selector:@selector(easySelected:)];
+        easy.position = ccp(-160.0, 315.0);
+        normal = [CCMenuItemImage itemFromNormalImage:@"normal.png" selectedImage:@"normal.png" target:self selector:@selector(normalSelected:)];
+        normal.position = ccp(-160.0, 240.0);
+        hard = [CCMenuItemImage itemFromNormalImage:@"hard.png" selectedImage:@"hard.png" target:self selector:@selector(hardSelected:)];
+        hard.position = ccp(-160.0, 165.0);
+        CCMenu *menu = [CCMenu menuWithItems:playgame, highscores, tutorial, easy, normal, hard, nil];
         menu.position = CGPointZero;
         [self addChild:menu];
     }
@@ -40,7 +50,40 @@
 -(void) playGameSelected:(id)sender
 {
     [[SimpleAudioEngine sharedEngine] playEffect:@"button.wav"];
-    [[CCDirector sharedDirector] pushScene:[CCTransitionCrossFade transitionWithDuration:1.0 scene:[SnakeGameLayer scene]]];
+    playgame.position = ccp(playgame.position.x+320.0, playgame.position.y);
+    highscores.position = ccp(highscores.position.x+320.0, highscores.position.y);
+    tutorial.position = ccp(tutorial.position.x+320.0, tutorial.position.y);
+    easy.position = ccp(easy.position.x+320.0, easy.position.y);
+    normal.position = ccp(normal.position.x+320.0, normal.position.y);
+    hard.position = ccp(hard.position.x+320.0, hard.position.y);
+}
+
+-(void) highScoresSelected:(id)sender
+{
+    
+}
+
+-(void) tutorialSelected:(id)sender
+{
+    
+}
+
+-(void) easySelected:(id)sender
+{
+    [[SimpleAudioEngine sharedEngine] playEffect:@"button.wav"];
+    [[CCDirector sharedDirector] replaceScene:[CCTransitionCrossFade transitionWithDuration:1.0 scene:[SnakeGameLayer scene]]];
+}
+
+-(void) normalSelected:(id)sender
+{
+    [[SimpleAudioEngine sharedEngine] playEffect:@"button.wav"];
+    [[CCDirector sharedDirector] replaceScene:[CCTransitionCrossFade transitionWithDuration:1.0 scene:[SnakeGameLayer scene]]];
+}
+
+-(void) hardSelected:(id)sender
+{
+    [[SimpleAudioEngine sharedEngine] playEffect:@"button.wav"];
+    [[CCDirector sharedDirector] replaceScene:[CCTransitionCrossFade transitionWithDuration:1.0 scene:[SnakeGameLayer scene]]];
 }
 
 @end
